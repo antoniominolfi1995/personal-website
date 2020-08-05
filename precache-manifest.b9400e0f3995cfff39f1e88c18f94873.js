8aff7cace208d19ac86bc4be310123a72cda4d8b
antoniominolfi <antoniominolfi@icloud.com>
2020-08-05 17:22:10 +0200

    Updates

---
+++ precache-manifest.b9400e0f3995cfff39f1e88c18f94873.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5abe2b13097bc27ac075e57171490f",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "048dd6b8961ecc80f16b",
+    "url": "/personal-website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "31c9b983ff308e1113d7",
+    "url": "/personal-website/static/js/2.adfa4518.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-website/static/js/2.adfa4518.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "048dd6b8961ecc80f16b",
+    "url": "/personal-website/static/js/main.cb69d0f1.chunk.js"
+  },
+  {
+    "revision": "d9385b05e5e57d9628fe",
+    "url": "/personal-website/static/js/runtime-main.3731dab7.js"
+  }
+]);
