8fd44f6790347c1e5e553de9ca53def6428dad4b
antoniominolfi <antoniominolfi@icloud.com>
2020-08-06 16:43:03 +0200

    inital setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+import { Contact } from "./components/Contact";
+import { NoMatch } from "./components/NoMatch";
+import { Layout } from "./components/Layout";
+import { NavigationBar } from "./components/NavigationBar";
+import { Jumbotron } from "./components/Jumbotron";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Antonio Minolfi official website (TEST)</h1>
-    </div>
+    <>
+      <NavigationBar />
+      <Jumbotron />
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </Layout>
+    </>
   );
 }
 
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Jumbotron as Jumbo, Container} from 'react-bootstrap';
+import styled from 'styled-components';
+import image from '../assets/winter.jpg';
+
+const Styles = styled.div`
+
+    .jumbo {
+        background: url(${image}) no-repeat fixed bottom;
+        background-size: cover;
+        color: black;
+        height: 200px;
+        position: relative;
+        z-index: -2;
+
+    }
+
+    .overlay {
+        background-color: white;
+        opacity: 0.6;
+        position: absolute;
+        top: 0; left: 0; bottom: 0; right: 0;
+        z-index: -1;
+    }
+
+
+
+`;
+
+export const Jumbotron = () => (
+    <Styles>
+        <Jumbo fluid className="jumbo">
+            <div className="overlay">
+                <Container>
+                    <h1>Welcome!</h1>
+                    <p>Check my new website</p>
+                </Container>
+            </div>
+        </Jumbo>
+    </Styles>
+
+)
+
+export default Jumbotron;
+
+
